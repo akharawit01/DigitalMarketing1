abc77af59151dfae65c0782e8a3150fd92cc4f84
AkharawitNt <akharawitbuzzwoo@gmail.com>
2018-03-07 01:38:13 +0700

    first commit and success

---
+++ assets/js/app.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $(".rslides").responsiveSlides();
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 2,
+            },
+            1000:{
+                items: 3,
+            }
+        }
+    });
+
+    $('a.nav-link').add('.warp-cont').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top - 80
+        }, 500);
+        return false;
+    });
+});
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }else {
+            $('#modalValid').modal('show')
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+})();
+
